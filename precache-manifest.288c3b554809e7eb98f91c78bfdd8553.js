c768fb3fb771112927b34e94ff872fc0685b2758
Daniel <daniel.frischling@gmx.de>
2020-10-16 10:00:49 +0200

    Updates

---
+++ precache-manifest.288c3b554809e7eb98f91c78bfdd8553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81566b60246b0e3cf7fbe21b607034d",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "48858f25c9d4613232c3",
+    "url": "/test-app/static/css/main.4810ece5.chunk.css"
+  },
+  {
+    "revision": "8d600eccaa0f92ec36a7",
+    "url": "/test-app/static/js/2.530d95a5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-app/static/js/2.530d95a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48858f25c9d4613232c3",
+    "url": "/test-app/static/js/main.4f917c6b.chunk.js"
+  },
+  {
+    "revision": "010976abd684515c15d4",
+    "url": "/test-app/static/js/runtime-main.107bf183.js"
+  }
+]);
